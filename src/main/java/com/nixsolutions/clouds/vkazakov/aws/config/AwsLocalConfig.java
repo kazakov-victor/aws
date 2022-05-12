af0546bc09138fc652eee34a8376fe24e5977221
kazakov.v <viktor.kazakov@nixsolutions.com>
2022-05-12 10:02:17 +0300

    Added separate config for a local profile

--- src/main/java/com/nixsolutions/clouds/vkazakov/aws/config/AwsLocalConfig.java
+++ src/main/java/com/nixsolutions/clouds/vkazakov/aws/config/AwsLocalConfig.java
@@ -1,5 +1,7 @@
 package com.nixsolutions.clouds.vkazakov.aws.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
@@ -9,24 +11,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 @RequiredArgsConstructor
-@Profile("!local")
-public class AwsConfig {
+@Profile("local")
+public class AwsLocalConfig {
     private final AwsConstants awsConstants;
 
     @Bean
     public AmazonS3 createS3() {
         return AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider
+                (new BasicAWSCredentials(
+                    awsConstants.getAccessKeyId(),
+                    awsConstants.getSecretKey())))
             .withRegion(awsConstants.getRegion()).build();
     }
 
     @Bean
     public AWSCognitoIdentityProvider createCognitoClient() {
         return AWSCognitoIdentityProviderClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                awsConstants.getAccessKeyId(), awsConstants.getSecretKey())))
             .withRegion(awsConstants.getRegion())
             .build();
     }
@@ -34,6 +44,9 @@ public class AwsConfig {
     @Bean
     public SnsClient createSnsClient() {
         return SnsClient.builder()
+            .credentialsProvider(StaticCredentialsProvider.create(
+                AwsBasicCredentials
+                    .create(awsConstants.getAccessKeyId(), awsConstants.getSecretKey())))
             .region(Region.of(awsConstants.getRegion()))
             .build();
     }
